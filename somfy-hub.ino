6b5e2901d943dfb97d14d2bb1aa546da319c7833
John Judeh <johnjudeh@hotmail.com>
2024-03-16 21:04:48 +0000

    format: Cleanup main file with consistent formatting

--- somfy-hub.ino
+++ somfy-hub.ino
@@ -1,8 +1,8 @@
 /*   This sketch allows you to emulate a Somfy RTS or Simu HZ remote.
    If you want to learn more about the Somfy RTS protocol, check out https://pushstack.wordpress.com/somfy-rts-protocol/
-   
+
    The rolling code will be stored in EEPROM, so that you can power the Arduino off.
-   
+
    Easiest way to make it work for you:
     - Choose a remote number
     - Choose a starting point for the rolling code. Any unsigned int works, 1 is a good start
@@ -10,7 +10,7 @@
     - Long-press the program button of YOUR ACTUAL REMOTE until your blind goes up and down slightly
     - send 'p' to the serial terminal
   To make a group command, just repeat the last two steps with another blind (one by one)
-  
+
   Then:
     - m, u or h will make it to go up
     - s make it stop
@@ -43,27 +43,27 @@ bool programmed;
 byte frame[7];
 byte checksum;
 
-void BuildFrame(byte *frame, byte button);
-void SendCommand(byte *frame, byte sync);
+void BuildFrame(byte* frame, byte button);
+void SendCommand(byte* frame, byte sync);
 
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println("");
-  
+
   pinMode(RF_DATA_OUT_PIN, OUTPUT);
   digitalWrite(RF_DATA_OUT_PIN, LOW);
-  
+
   pinMode(BTN_UP_PIN, INPUT);
   digitalWrite(BTN_UP_PIN, LOW);
-  
+
   pinMode(BTN_STOP_PIN, INPUT);
   digitalWrite(BTN_STOP_PIN, LOW);
-  
+
   pinMode(BTN_DOWN_PIN, INPUT);
   digitalWrite(BTN_DOWN_PIN, LOW);
-  
+
   if (EEPROM.begin(EEPROM_TOTAL_MEMORY)) {
     Serial.println("EEPROM initialised");
   }
@@ -72,7 +72,8 @@ void setup() {
   EEPROM.get(EEPROM_ADDR_ROLLING_CODE, rollingCode);
   if (EEPROM.read(EEPROM_ADDR_PROGRAMMED) == 0xFF) {
     programmed = false;
-  } else {
+  }
+  else {
     EEPROM.get(EEPROM_ADDR_PROGRAMMED, programmed);
   }
   Serial.print("Initial rolling code    : "); Serial.println(rollingCode);
@@ -84,7 +85,7 @@ void setup() {
     EEPROM.put(EEPROM_ADDR_ROLLING_CODE, rollingCode);
     EEPROM.commit();
   }
-  
+
   Serial.print("Simulated remote number : "); Serial.println(REMOTE, HEX);
   Serial.print("Current rolling code    : "); Serial.println(rollingCode);
 }
@@ -95,9 +96,9 @@ void loop() {
   byte btnDown = digitalRead(BTN_DOWN_PIN);
 
   bool btnActivated = btnUp == HIGH || btnStop == HIGH || btnDown == HIGH;
-  
+
   char serie = '\0';
-  
+
   if (Serial.available() > 0) {
     serie = (char)Serial.read();
     Serial.println("");
@@ -105,19 +106,19 @@ void loop() {
   }
 
   if (serie != '\0' || btnActivated) {
-    if(serie == 'm'||serie == 'u'||serie == 'h' || btnUp == HIGH) {
+    if (serie == 'm' || serie == 'u' || serie == 'h' || btnUp == HIGH) {
       Serial.println("Monte"); // Somfy is a French company, after all.
       BuildFrame(frame, UP);
     }
-    else if(serie == 's' || btnStop == HIGH) {
+    else if (serie == 's' || btnStop == HIGH) {
       Serial.println("Stop");
       BuildFrame(frame, STOP);
     }
-    else if(serie == 'b'||serie == 'd' || btnDown == HIGH) {
+    else if (serie == 'b' || serie == 'd' || btnDown == HIGH) {
       Serial.println("Descend");
       BuildFrame(frame, DOWN);
     }
-    else if(serie == 'p') {
+    else if (serie == 'p') {
       Serial.println("Prog");
       BuildFrame(frame, PROG);
     }
@@ -125,14 +126,14 @@ void loop() {
       Serial.println("Custom code");
       BuildFrame(frame, serie);
     }
-  
+
     Serial.println("");
     SendCommand(frame, 2);
-    for(int i = 0; i<2; i++) {
+    for (int i = 0; i < 2; i++) {
       SendCommand(frame, 7);
     }
-  
-    if(serie == 'p' and !programmed) {
+
+    if (serie == 'p' and !programmed) {
       programmed = true;
       EEPROM.put(EEPROM_ADDR_PROGRAMMED, programmed);
       EEPROM.commit();
@@ -141,59 +142,59 @@ void loop() {
 }
 
 
-void BuildFrame(byte *frame, byte button) {
+void BuildFrame(byte* frame, byte button) {
   frame[0] = 0xA7;             // Encryption key. Doesn't matter much
   frame[1] = button << 4;      // Which button did  you press? The 4 LSB will be the checksum
   frame[2] = rollingCode >> 8; // Rolling code (big endian)
   frame[3] = rollingCode;      // Rolling code
   frame[4] = REMOTE >> 16;     // Remote address
-  frame[5] = REMOTE >>  8;     // Remote address
+  frame[5] = REMOTE >> 8;     // Remote address
   frame[6] = REMOTE;           // Remote address
 
   Serial.print("Frame         : ");
-  for(byte i = 0; i < 7; i++) {
-    if(frame[i] >> 4 == 0) { //  Displays leading zero in case the most significant
+  for (byte i = 0; i < 7; i++) {
+    if (frame[i] >> 4 == 0) { //  Displays leading zero in case the most significant
       Serial.print("0");     // nibble is a 0.
     }
-    Serial.print(frame[i],HEX); Serial.print(" ");
+    Serial.print(frame[i], HEX); Serial.print(" ");
   }
-  
-// Checksum calculation: a XOR of all the nibbles
+
+  // Checksum calculation: a XOR of all the nibbles
   checksum = 0;
-  for(byte i = 0; i < 7; i++) {
+  for (byte i = 0; i < 7; i++) {
     checksum = checksum ^ frame[i] ^ (frame[i] >> 4);
   }
   checksum &= 0b1111; // We keep the last 4 bits only
 
 
-//Checksum integration
+  //Checksum integration
   frame[1] |= checksum; //  If a XOR of all the nibbles is equal to 0, the blinds will
-                        // consider the checksum ok.
+  // consider the checksum ok.
 
   Serial.println(""); Serial.print("With checksum : ");
-  for(byte i = 0; i < 7; i++) {
-    if(frame[i] >> 4 == 0) {
+  for (byte i = 0; i < 7; i++) {
+    if (frame[i] >> 4 == 0) {
       Serial.print("0");
     }
-    Serial.print(frame[i],HEX); Serial.print(" ");
+    Serial.print(frame[i], HEX); Serial.print(" ");
   }
 
-  
-// Obfuscation: a XOR of all the bytes
-  for(byte i = 1; i < 7; i++) {
-    frame[i] ^= frame[i-1];
+
+  // Obfuscation: a XOR of all the bytes
+  for (byte i = 1; i < 7; i++) {
+    frame[i] ^= frame[i - 1];
   }
 
   Serial.println(""); Serial.print("Obfuscated    : ");
-  for(byte i = 0; i < 7; i++) {
-    if(frame[i] >> 4 == 0) {
+  for (byte i = 0; i < 7; i++) {
+    if (frame[i] >> 4 == 0) {
       Serial.print("0");
     }
-    Serial.print(frame[i],HEX); Serial.print(" ");
+    Serial.print(frame[i], HEX); Serial.print(" ");
   }
   Serial.println("");
   Serial.print("Rolling Code  : "); Serial.println(rollingCode);
-  
+
   //  We store the value of the rolling code in the
   // EEPROM. It should take up to 2 adresses but the
   // Arduino function takes care of it.
@@ -204,33 +205,33 @@ void BuildFrame(byte *frame, byte button) {
 
 
 
-void SendCommand(byte *frame, byte sync) {
-  if(sync == 2) { // Only with the first frame.
-  //Wake-up pulse & Silence
+void SendCommand(byte* frame, byte sync) {
+  if (sync == 2) { // Only with the first frame.
+    //Wake-up pulse & Silence
     digitalWrite(RF_DATA_OUT_PIN, HIGH);
     delayMicroseconds(9415);
     digitalWrite(RF_DATA_OUT_PIN, LOW);
     delayMicroseconds(89565);
   }
 
-// Hardware sync: two sync for the first frame, seven for the following ones.
+  // Hardware sync: two sync for the first frame, seven for the following ones.
   for (int i = 0; i < sync; i++) {
     digitalWrite(RF_DATA_OUT_PIN, HIGH);
-    delayMicroseconds(4*SYMBOL);
+    delayMicroseconds(4 * SYMBOL);
     digitalWrite(RF_DATA_OUT_PIN, LOW);
-    delayMicroseconds(4*SYMBOL);
+    delayMicroseconds(4 * SYMBOL);
   }
 
-// Software sync
+  // Software sync
   digitalWrite(RF_DATA_OUT_PIN, HIGH);
   delayMicroseconds(4550);
   digitalWrite(RF_DATA_OUT_PIN, LOW);
   delayMicroseconds(SYMBOL);
-  
-  
-//Data: bits are sent one by one, starting with the MSB.
-  for(byte i = 0; i < 56; i++) {
-    if(((frame[i/8] >> (7 - (i%8))) & 1) == 1) {
+
+
+  //Data: bits are sent one by one, starting with the MSB.
+  for (byte i = 0; i < 56; i++) {
+    if (((frame[i / 8] >> (7 - (i % 8))) & 1) == 1) {
       digitalWrite(RF_DATA_OUT_PIN, LOW);
       delayMicroseconds(SYMBOL);
       digitalWrite(RF_DATA_OUT_PIN, HIGH);
@@ -243,7 +244,7 @@ void SendCommand(byte *frame, byte sync) {
       delayMicroseconds(SYMBOL);
     }
   }
-  
+
   digitalWrite(RF_DATA_OUT_PIN, LOW);
   delayMicroseconds(30415); // Inter-frame silence
 }
